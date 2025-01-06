e85853cd71046fdfe5d37fa40b276fd114a5813d
MohitVirani <mohitvirani2200@gmail.com>
2025-01-06 12:27:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+// const app = require('./app')
+
+// console.log(app.x)
+
+
+// const http = require('http');
+
+// function dataControl(req,resp){
+//     resp.write("<h1>Code Step by Step</h1>");;
+//     resp.end();
+// }
+
+// http.createServer(dataControl).listen(4500);
+
+// var colors = require('colors');
+// console.log('hello'.bgBlue);
+// console.log('hello'.green); // outputs green text
+// console.log('i like cake and pies'.underline.red) // outputs red underlined text
+// console.log('inverse the color'.inverse); // inverses the color
+// console.log('OMG Rainbows!'.rainbow); // rainbow
+// console.log('Run the trap'.trap); // Drops the bass
+
+// import chalk from 'chalk';
+
+// const log = console.log;
+
+// // Combine styled and normal strings
+// log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+// // Compose multiple styles using the chainable API
+// log(chalk.green.bgRed.bold('Hello world!'));
+
+// // Pass in multiple arguments
+// log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+
+// // Nest styles
+// log(chalk.red('Hello', chalk.underline.red('world') + '!'));
+
+// // Nest styles of the same type even (color, underline, background)
+// log(chalk.green(
+// 	'I am a green line ' +
+// 	chalk.blue.underline.bold('with a blue substring') +
+// 	' that becomes green again!'
+// ));
+
+// // ES2015 template literal
+// log(`
+// CPU: ${chalk.red('90%')}
+// RAM: ${chalk.green('40%')}
+// DISK: ${chalk.yellow('70%')}
+// `);
+
+// // Use RGB colors in terminal emulators that support it.
+// log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
+// log(chalk.hex('#DEADED').bold('Bold gray!'));
