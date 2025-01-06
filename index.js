efd6d11c6c618b6a76fe84aac2868b03512d00cb
MohitVirani <mohitvirani2200@gmail.com>
2025-01-06 16:04:48 +0530

    commit

--- index.js
+++ index.js
@@ -62,11 +62,3 @@
 //     resp.write(JSON.stringify(data));
 //     resp.end();
 // }).listen(2200)
-// const http = require('http');
-// const data = require('./data')
-
-// http.createServer((req,resp)=>{
-//     resp.writeHead(200,{'Content-Type':'application\json'});
-//     resp.write(JSON.stringify(data));
-//     resp.end();
-// }).listen(2200)
