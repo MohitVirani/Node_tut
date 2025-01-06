422492f734a335e7f8912ff2ec798c861fbdf6ef
MohitVirani <mohitvirani2200@gmail.com>
2025-01-06 16:57:45 +0530

    commit

--- index.js
+++ index.js
@@ -62,3 +62,29 @@
 //     resp.write(JSON.stringify(data));
 //     resp.end();
 // }).listen(2200)
+
+// const fs = require('fs');
+
+// const input = process.argv;
+
+// if(input[2] =='add'){
+//     fs.writeFileSync(input[3],input[4])
+// }
+// else if(input[2] == 'remove'){
+//     fs.unlinkSync(input[3]);
+// }
+// else{
+//     console.log("invalid input")
+// }
+
+const fs = require('fs');
+const path = require('path');
+const DirPath = path.join(__dirname,'files')
+
+console.warn(DirPath)
+
+for(i = 0;i<5;i++){
+    fs.writeFileSync(DirPath+`/hello${i}.txt`,'a simple text file')
+}
+
+//fs.writeFileSync('Apple.txt','This Is apple file')
